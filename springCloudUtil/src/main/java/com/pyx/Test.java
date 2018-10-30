a8e6d4ae9d8e7ec59746ae8a42ed0d6f0fef034b
piyingxu <516609723@qq.com>
2018-10-30 18:08:14 +0800

    maven私有服nexus测试公共包

---
+++ springCloudUtil/src/main/java/com/pyx/Test.java
@@ -0,0 +1,17 @@
+package com.pyx;
+
+/**
+ * @author: yingxu.pi@transsnet.com
+ * @date: 2018/10/30 10:16
+ */
+public class Test {
+
+    public void test () {
+        System.out.println("hellow Nexus");
+    }
+
+    public void test1 () {
+        System.out.println("hellow Nexus good");
+    }
+
+}
---
+++ springCloudUtil/src/main/java/com/pyx/Util.java
@@ -0,0 +1,12 @@
+package com.pyx;
+
+/**
+ * @author: yingxu.pi@transsnet.com
+ * @date: 2018/10/30 13:50
+ */
+public class Util {
+
+    public void test () {
+        System.out.println("hellow Util pppppppp");
+    }
+}
