11ef599eaa119e459acb891cfde61faecce1ef2e
piyingxu <516609723@qq.com>
2018-10-30 18:25:07 +0800

    配置中心

---
+++ config-center/src/main/java/com/piyingxu/ConfigApplication.java
@@ -0,0 +1,22 @@
+package com.piyingxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+        System.out.println("config center is start===");
+    }
+
+}
---
+++ config-center/src/main/java/com/piyingxu/control/WebController.java
@@ -0,0 +1,23 @@
+/*
+package com.piyingxu.control;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	private Logger logger = LoggerFactory.getLogger(WebController.class);
+
+	@RequestMapping(value = "/pp", method = RequestMethod.GET)
+	public String health() {
+		return "hello health ";
+	}
+
+
+
+}
+*/
